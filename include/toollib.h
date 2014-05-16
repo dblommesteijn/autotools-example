8fda93e1290d43c8162a73b36faa2a65d68d9e6e
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-05-16 09:01:52 +0200

    changes

---
+++ include/toollib.h
@@ -0,0 +1,9 @@
+#ifndef TOOLLIB_H_
+#define TOOLLIB_H_
+
+#include <iostream>
+using namespace std;
+
+int toollib(string input);
+
+#endif
---
+++ src/toollib.cpp
@@ -0,0 +1,10 @@
+#include "../include/toollib.h"
+
+#include <iostream>
+using namespace std;
+
+int toollib(string input){
+	cout << input << endl;
+	return 0;
+}
+
